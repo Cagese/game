e9b0ea3899c7ca29c56543cbe16db2311f81f83d
nonamefo <bariseva24@gmail.com>
2024-01-03 00:24:40 +0300

    подчистил код и добавил возможность изменить резмер персонажа просто поменяв переменную size некоторые ошибки PEP8 оставил для более простой читаемости кода,так же теперь приложение открываеться в полноекранном режиме и размеры окна передаются в переменную screen_size = width, height так же изменил некоторые функции котоые получали аргументы которые уже были в их зоне видемости (ну область видемости переменных такие переменные как hero необязательно указывать так как функции уже их видят и может произойти конфликт переменных)

--- game.py
+++ game.py
@@ -6,6 +6,10 @@ from pygame.sprite import Sprite
 
 pygame.init()
 
+# используеться для загрузки изображений
+# в случаи неправельной загрузки изображения
+# генерируеться исключение не остонавливающее работу программы
+
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
@@ -25,6 +29,7 @@ def load_image(name, color_key=None):
 class SpriteGroup(pygame.sprite.Group):
 
     def __init__(self):
+        # наследование класса из pygame.sprite.Group
         super().__init__()
 
     def get_event(self, event):
@@ -34,6 +39,7 @@ class SpriteGroup(pygame.sprite.Group):
 
 class Camera:
     def __init__(self):
+        # задаём начальные координаты камеры
         self.dx = 0
         self.dy = 0
 
@@ -42,20 +48,22 @@ class Camera:
         obj.rect.y = self.dy
 
     def update(self, target):
-        x,y = target.pos
+        # отслеживание передвижения и смешение камеры
+        x, y = target.pos
         self.dx = x
         self.dy = y
 
 
+# отслеживание нажатия клавиш и в какую сторону надо двигаться
 def movement_to_direction(movement):
-    if movement == [0, 1]: return 'up'
-    if movement == [0, -1]: return 'down'
-    if movement == [1, 0]: return 'left'
-    if movement == [-1, 0]: return 'right'
+    if movement == [0, 1]:    return 'up'
+    if movement == [0, -1]:   return 'down'
+    if movement == [1, 0]:    return 'left'
+    if movement == [-1, 0]:   return 'right'
 
 
 class Player(Sprite):
-    def __init__(self, pos_x, pos_y, movement=None, direction='down'):
+    def __init__(self, pos_x, pos_y, movement=None):
         super().__init__(hero_group)
         if movement is None:
             movement = [0, 0]
@@ -66,46 +74,54 @@ class Player(Sprite):
         self.pos = (pos_x - 120, pos_y - 120)
         self.step = 0
 
-    def move(self, x, y):
+    def move(self, x, y, size_obj):
         if sum(map(abs, self.movement)) == 0:
-            player_animation(self, 'idle', 16)
+            player_animation(self, 'idle', 16, size_obj)
         else:
             self.pos = (x, y)
-            player_animation(self, 'walk', 16)
+            player_animation(self, 'walk', 16, size_obj)
         self.update()
 
 
-def player_animation(player, type_move, player_fps):
+def player_animation(player, type_move, player_fps, size_obj):
+
     player.step = (player.step + 1) % (player_fps * 4)
     if sum(map(abs, player.movement)) == 2:
         if player.movement[1] == 1:
             player.image = pygame.transform.scale(
                 load_image(
-                    f'player\Character without weapon\{type_move}\{type_move} up{player.step // player_fps + 1}.png'),
-                (256, 256))
+                    f'player\
+                    Character without weapon\
+                    {type_move}\
+                    {type_move} up{player.step // player_fps + 1}.png'),
+                size_obj)
         else:
             player.image = pygame.transform.scale(
                 load_image(
-                    f'player\Character without weapon\{type_move}\{type_move} down{player.step // player_fps + 1}.png'),
-                (256, 256))
+                    f'player\
+                    Character without weapon\
+                    {type_move}\
+                    {type_move} down{player.step // player_fps + 1}.png'),
+                size_obj)
     else:
         if sum(map(abs, player.movement)) != 0:
             player.direction = movement_to_direction(player.movement)
         player.image = pygame.transform.scale(
             load_image(
-                f'player\Character without weapon\{type_move}\{type_move} {player.direction}{player.step // player_fps + 1}.png'),
-            (256, 256))
+                f'player\
+                Character without weapon\
+                {type_move}\
+                {type_move} {player.direction}{player.step // player_fps + 1}.png'),
+            size_obj)
 
 
-
-
-def move(hero):
+def move(size_obj):
     speed = 5
     x, y = hero.pos
     movement = hero.movement
     x = movement[0] * speed + x
     y = movement[1] * speed + y
-    hero.move(x, y)
+    hero.move(x, y, size_obj)
 
 
 def generate_background():
@@ -117,9 +133,11 @@ def generate_background():
             screen.blit(background, (x, y))
 
 
-screen_size = width, height = (1000, 800)
 map_size = (100, 100)
-screen = pygame.display.set_mode(screen_size)
+size = (256, 256)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen_size = width, height = (pygame.display.get_surface().get_width(), pygame.display.get_surface().get_height())
+
 clock = pygame.time.Clock()
 sprite_group = SpriteGroup()
 hero_group = SpriteGroup()
@@ -127,19 +145,15 @@ camera = Camera()
 FPS = 60
 running = True
 background = load_image(f'ground/{random.randint(1, 6)}.png')
-player_image = pygame.transform.scale(load_image('player/Character without weapon/idle/idle down1.png'), (256, 256))
+player_image = pygame.transform.scale(load_image('player/Character without weapon/idle/idle down1.png'), (64, 64))
 hero = Player(width // 2, height // 2)
 
 
-
-#Тестовый объект для проверки отресовки спрайтов с учётом камеры (можно удалить)
-test = pygame.transform.scale(load_image('player/Character without weapon/idle/idle down1.png'), (256, 256))
-test_obj = pygame.sprite.Sprite(sprite_group)
-test_obj.image = test
-test_obj.rect = test_obj.image.get_rect()
-
-
-
+# Тестовый объект для проверки отресовки спрайтов с учётом камеры (можно удалить)
+# test = pygame.transform.scale(load_image('player/Character without weapon/idle/idle down1.png'), (64, 64))
+# test_obj = pygame.sprite.Sprite(sprite_group)
+# test_obj.image = test
+# test_obj.rect = test_obj.image.get_rect()
 
 
 while running:
@@ -166,7 +180,7 @@ while running:
             elif event.key == pygame.K_d:
                 hero.movement[0] = 0
     generate_background()
-    move(hero)
+    move(size)
     camera.update(hero)
     for sprite in sprite_group:
         camera.apply(sprite)
