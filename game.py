385a269304e065c52efe5389e08c8d4b02046e4d
mikha <mikhail_morozov@mail.ru>
2024-01-04 04:13:34 +0300

    добавил спрайты для врагов, переделал систему координат камеры, прописал алгоритм пути врагов к герою,прописал класс врагов без атаки и анимаций(пока что)

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ from pygame.sprite import Sprite
 
 pygame.init()
 
+
 # используеться для загрузки изображений
 # в случаи неправельной загрузки изображения
 # генерируеться исключение не остонавливающее работу программы
@@ -44,14 +45,13 @@ class Camera:
         self.dy = 0
 
     def apply(self, obj):
-        obj.rect.x = self.dx
-        obj.rect.y = self.dy
+        obj.rect.x = (-obj.pos[0] + self.dx)
+        obj.rect.y = (-obj.pos[1] + self.dy)
 
     def update(self, target):
         # отслеживание передвижения и смешение камеры
-        x, y = target.pos
-        self.dx = x
-        self.dy = y
+        self.dx = target.pos_x + width // 2 - target.rect.w * 2
+        self.dy = target.pos_y + height // 2 - target.rect.h * 2
 
 
 # отслеживание нажатия клавиш и в какую сторону надо двигаться
@@ -63,56 +63,79 @@ def movement_to_direction(movement):
 
 
 class Player(Sprite):
-    def __init__(self, pos_x, pos_y, movement=None):
-        super().__init__(hero_group)
-        if movement is None:
-            movement = [0, 0]
+    def __init__(self, pos_x, pos_y):
+        super().__init__(all_sprites, hero_group)
         self.image = player_image
-        self.movement = movement
+        self.movement = [0, 0]
         self.direction = 'down'
-        self.rect = self.image.get_rect().move(pos_x - 120, pos_y - 120)
-        self.pos = (pos_x - 120, pos_y - 120)
+        self.rect = self.image.get_rect().move(pos_x - size[0] // 2, pos_y - size[1] // 2)
+        self.pos = (pos_x - size[0] // 2, pos_y - size[1] // 2)
+        self.pos_x, self.pos_y = pos_x, pos_y
         self.step = 0
 
     def move(self, x, y, size_obj):
         if sum(map(abs, self.movement)) == 0:
-            player_animation(self, 'idle', 16, size_obj)
+            player_animation(self, 'idle', 'with sword and shield', 16, size_obj)
         else:
             self.pos = (x, y)
-            player_animation(self, 'walk', 16, size_obj)
+            self.pos_x, self.pos_y = (x, y)
+            player_animation(self, 'walk', 'with sword and shield', 16, size_obj)
+
         self.update()
 
 
-def player_animation(player, type_move, player_fps, size_obj):
-
+def player_animation(player, type_move, type_sprite, player_fps, size_obj):
     player.step = (player.step + 1) % (player_fps * 4)
     if sum(map(abs, player.movement)) == 2:
         if player.movement[1] == 1:
             player.image = pygame.transform.scale(
                 load_image(
-                    f'player\
-                    Character without weapon\
-                    {type_move}\
-                    {type_move} up{player.step // player_fps + 1}.png'),
+                    f'player\Character {type_sprite}\{type_move}\{type_move} up{player.step // player_fps + 1}.png'),
                 size_obj)
         else:
             player.image = pygame.transform.scale(
                 load_image(
-                    f'player\
-                    Character without weapon\
-                    {type_move}\
-                    {type_move} down{player.step // player_fps + 1}.png'),
+                    f'player\Character {type_sprite}\{type_move}\{type_move} down{player.step // player_fps + 1}.png'),
                 size_obj)
     else:
         if sum(map(abs, player.movement)) != 0:
             player.direction = movement_to_direction(player.movement)
         player.image = pygame.transform.scale(
             load_image(
-                f'player\
-                Character without weapon\
-                {type_move}\
-                {type_move} {player.direction}{player.step // player_fps + 1}.png'),
+                f'player\Character {type_sprite}\{type_move}\{type_move} {player.direction}{player.step // player_fps + 1}.png'),
             size_obj)
+
+
+class Enemy(Sprite):
+    def __init__(self):
+        super().__init__(all_sprites, enemy_group)
+        self.type = ['zombie', 'skeleton'][random.randint(0, 1)]
+        self.image = pygame.transform.scale(load_image(f'enemy\skeleton\walk\left_walk1.png'),
+                                            tuple(map(lambda x: x // 2, list(size))))
+        self.movement = [0, 0]
+        self.direction = 'down'
+        self.pos_x, self.pos_y = ((hero.pos[0] + random.randint(20, 200) * random.choice([-1, 1])),
+                                  (hero.pos[1] + random.randint(20, 200) * random.choice([-1, 1])))
+        print(self.pos_x, self.pos_y, hero.pos)
+        self.rect = self.image.get_rect().move(self.pos_x, self.pos_y)
+        self.pos = (self.pos_x, self.pos_y)
+        self.step = 0
+
+    def move(self, speed):
+        x1, y1 = self.pos
+        x2, y2 = hero.pos
+        x2, y2 = x2 - hero.rect.w, y2 - hero.rect.h
+        if abs(x2 - x1) > 40:
+            if x1 > x2:
+                x1 -= speed
+            else:
+                x1 += speed
+        if abs(y2 - y1) > 40:
+            if y1 > y2:
+                y1 -= speed
+            else:
+                y1 += speed
+        self.pos = (x1, y1)
 
 
 def move(size_obj):
@@ -139,30 +162,35 @@ screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screen_size = width, height = (pygame.display.get_surface().get_width(), pygame.display.get_surface().get_height())
 
 clock = pygame.time.Clock()
-sprite_group = SpriteGroup()
+all_sprites = SpriteGroup()
 hero_group = SpriteGroup()
-camera = Camera()
+enemy_group = SpriteGroup()
+
 FPS = 60
 running = True
 background = load_image(f'ground/{random.randint(1, 6)}.png')
 player_image = pygame.transform.scale(load_image('player/Character without weapon/idle/idle down1.png'), (64, 64))
 hero = Player(width // 2, height // 2)
-
+camera = Camera()
 
 # Тестовый объект для проверки отресовки спрайтов с учётом камеры (можно удалить)
 # test = pygame.transform.scale(load_image('player/Character without weapon/idle/idle down1.png'), (64, 64))
-# test_obj = pygame.sprite.Sprite(sprite_group)
+# test_obj = pygame.sprite.Sprite(all_sprites)
 # test_obj.image = test
 # test_obj.rect = test_obj.image.get_rect()
-
+test = 0
 
 while running:
+    test += 1
+    if test % (60 * 2) == 0:
+        Enemy()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_w:
                 hero.movement[1] = 1
             elif event.key == pygame.K_s:
                 hero.movement[1] = -1
@@ -182,9 +210,13 @@ while running:
     generate_background()
     move(size)
     camera.update(hero)
-    for sprite in sprite_group:
+    for sprite in enemy_group:
+        sprite.move(3)
+    for sprite in all_sprites:
         camera.apply(sprite)
-    sprite_group.draw(screen)
+    all_sprites.draw(screen)
     hero_group.draw(screen)
+
     pygame.display.flip()
+
     clock.tick(FPS)
