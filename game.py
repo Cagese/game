d2b3c427895cda330d9b954dee7f5636bc9c078c
mikha <mikhail_morozov@mail.ru>
2024-01-08 20:16:07 +0300

    переделал систему анимации, добавил смерть главного героя, атаку врагов

--- game.py
+++ game.py
@@ -80,6 +80,9 @@ class Player(Sprite):
 
         self.hitboxes = []
         self.is_attack = False
+        self.immunity = 0
+        self.max_hp = 100
+        self.hp = 100
 
         self.step = 0
 
@@ -106,8 +109,18 @@ class Player(Sprite):
             pygame.draw.rect(screen, pygame.Color('blue'), down_hitbox, width=5)
             pygame.draw.rect(screen, pygame.Color('green'), left_hitbox, width=5)
             pygame.draw.rect(screen, pygame.Color('purple'), right_hitbox, width=5)
+    def take_damage(self, damage):
+        if self.immunity <= 0:
+            self.hp -= damage
+            if self.hp <= 0:
+                self.kill()
+            self.immunity = 2
+            self.immunity_start_time = counter
+        else:
+            if self.immunity_start_time + self.immunity <= counter:
+                self.immunity = 0
 
-    def attack(self, size_obj):
+    def attack(self):
         attack_hitbox = self.rect.copy().move(size[0] // 2 - 32, size[1] // 2 - 32)
         for i in self.hitboxes:
             if self.direction == i[1]:
@@ -115,15 +128,26 @@ class Player(Sprite):
         if debug:
             pygame.draw.rect(screen, pygame.Color('red'), attack_hitbox, width=5)
         for enemy in enemy_group:
-            if attack_hitbox.colliderect(enemy.rect):
+            if attack_hitbox.colliderect(enemy.hitbox):
                 enemy.take_damage(50)
 
     def move(self, x, y, size_obj):
         self.update_hitboxes()
 
+        if self.hp > 0:
+            healthbar = self.hitboxes[0][0].copy()
+            healthbar.h = 5
+            healthbar.y -=10
+            healthbar_background = healthbar.copy()
+            healthbar_background.h = 5
+            healthbar.w = self.rect.w * (self.hp/self.max_hp)
+            pygame.draw.rect(screen, (128, 128, 128), healthbar_background)
+            pygame.draw.rect(screen, (255, 0, 0), healthbar)
+
+
         if self.is_attack:
             player_animation(self, 'attack', 'with sword and shield', 8, size_obj)
-            self.attack(size_obj)
+            self.attack()
             return 0
 
         if sum(map(abs, self.movement)) == 0:
@@ -159,10 +183,12 @@ def player_animation(player, type_move, type_sprite, player_fps, size_obj):
 class Enemy(Sprite):
     def __init__(self):
         super().__init__(all_sprites, enemy_group)
+        self.dead = False
         self.type = ['zombie', 'skeleton'][random.randint(0, 1)]
         self.size = tuple(map(lambda x: x // 2, list(size)))
         self.image = pygame.transform.scale(load_image(f'enemy\{self.type}\walk\left_walk1.png'),
                                             self.size)
+        self.last_update = pygame.time.get_ticks()
 
         self.direction = 'left'
         self.pos_x, self.pos_y = ((hero.pos[0] + random.randint(200, 500) * random.choice([-1, 1])),
@@ -173,32 +199,71 @@ class Enemy(Sprite):
         global enemy_max_health
         self.max_hp = enemy_max_health
         self.hp = enemy_max_health
+        self.dead = False
         self.show_healthbar = False
         self.immunity = 0
 
+        self.update_hitbox()
         self.step = 0
 
     def take_damage(self, damage):
         if self.immunity <= 0:
             self.hp -= damage
             if self.hp <= 0:
-                self.kill()
+                self.dead = True
             self.show_healthbar = True
             self.immunity = 1
+            self.immunity_start_time = counter
         else:
-            if counter % 2 == 0:
-                self.immunity -= 1
+            if self.immunity_start_time + self.immunity <= counter:
+                self.immunity = 0
+    def attack(self):
+        if self.type == 'skeleton':
+            enemy_animation(self,'attack',120,4)
+        elif self.type == 'zombie':
+            enemy_animation(self, 'attack', 120, 8)
+        hero.take_damage(enemy_strength)
+
+    def update_hitbox(self):
+        self.hitbox = self.rect.copy()
+        self.hitbox.x += self.hitbox.w // 4
+        self.hitbox.w //= 2
 
     def move(self, speed):
+        self.update_hitbox()
+
 
         if debug:
-            pygame.draw.rect(screen, (0, 0, 0), self.rect, width=5)
+            pygame.draw.rect(screen, (0, 0, 0), self.hitbox, width=5)
 
-        if self.show_healthbar:
+
+        if self.dead:
+            if self.type == 'skeleton':
+                if not(self.step%5 + 1 == 5):
+                    enemy_animation(self, 'dead', 120, 5)
+                else:
+                    self.kill()
+            elif self.type == 'zombie':
+                if not(self.step%8 + 1 == 8):
+                    enemy_animation(self, 'dead', 120, 8)
+                else:
+                    self.kill()
+            return 0
+
+        if self.show_healthbar and self.hp > 0:
             healthbar = self.rect.copy()
             healthbar.h = 5
-            healthbar.w = self.rect.w - self.hp
-            pygame.draw.rect(screen, (255, 0, 0, 150), healthbar)
+            healthbar_background = self.rect.copy()
+            healthbar_background.h = 5
+            healthbar.w = self.rect.w * (self.hp/self.max_hp)
+            pygame.draw.rect(screen, (128, 128, 128), healthbar_background)
+            pygame.draw.rect(screen, (255, 0, 0), healthbar)
+
+
+        for i in hero.hitboxes:
+            if self.hitbox.colliderect(i[0]):
+                self.attack()
+                return 0
 
         if len(pygame.sprite.spritecollide(self, enemy_group, False)) != 1:
             speed = random.randint(0, speed)
@@ -231,15 +296,20 @@ class Enemy(Sprite):
             Ox()
         else:
             Oy()
-        enemy_animation(self, 'walk', 16, 6)
+        enemy_animation(self, 'walk', 120, 6)
         self.pos = (x1, y1)
 
 
-def enemy_animation(enemy, type_move, enemy_fps, sprite_count):
-    enemy.step = (enemy.step + 1) % (sprite_count * sprite_count) + 1
+def enemy_animation(enemy, type_move, enemy_tick, sprite_count):
+    now = pygame.time.get_ticks()
+    if now - enemy.last_update > enemy_tick:
+        enemy.last_update = now
+        enemy.step = (enemy.step + 1) % sprite_count
+
+
     direction = enemy.direction
     enemy.image = pygame.transform.scale(
-        load_image(f'enemy\{enemy.type}\{type_move}\{direction}_{type_move}{enemy.step // enemy_fps + 1}.png'),
+        load_image(f'enemy\{enemy.type}\{type_move}\{direction}_{type_move}{enemy.step % sprite_count + 1}.png'),
         enemy.size)
 
 
@@ -286,6 +356,8 @@ counter = 0
 counter_font = pygame.font.SysFont('Consolas', 60)
 
 enemy_max_health = 100
+enemy_strength = 10
+difficulty = 20
 
 while running:
     for event in pygame.event.get():
@@ -294,7 +366,8 @@ while running:
         elif event.type == pygame.USEREVENT:
             counter += 1
             if counter % 30 == 0:
-                enemy_max_health *= 1.2
+                enemy_max_health *= 1 + difficulty/100
+                enemy_strength *= 1 + difficulty/100
             if counter % 2 == 0 and len(enemy_group) <= 100:
                 Enemy()
         elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -343,3 +416,4 @@ while running:
     pygame.display.flip()
 
     clock.tick(FPS)
+    print(hero.immunity)
